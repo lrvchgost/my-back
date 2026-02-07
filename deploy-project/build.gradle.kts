726a4843dbea4df1100f4565841e73c995453a00
Roman Lonskii <lrvchgost@gmail.com>
2026-02-07 17:11:22 +0300

    deploy

---
+++ deploy-project/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("build-jvm")
+    id("maven-publish")
+}
+
+group = "ru.otus.otuskotlin.lrvch.tests"
+version = "0.1.0"
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
+}
---
+++ deploy-project/deploy-project-dcompose/dcompose/keycloak-tokens.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+KCHOST=http://localhost:8080
+REALM=catalog-of-storages
+CLIENT_ID=catalog-of-storages-service
+UNAME=storage-test
+PASSWORD=pwd
+
+# shellcheck disable=SC2006
+#ACCESS_TOKEN=`curl \
+#  -d "client_id=$CLIENT_ID" -d "client_secret=$CLIENT_SECRET" \
+#  -d "username=$UNAME" -d "password=$PASSWORD" \
+#  -d "grant_type=password" \
+#  "$KCHOST/auth/realms/$REALM/protocol/openid-connect/token"  | jq -r '.access_token'`
+
+ACCESS_TOKEN=`curl \
+  -d "client_id=$CLIENT_ID" \
+  -d "username=$UNAME" \
+  -d "password=$PASSWORD" \
+  -d "grant_type=password" \
+  "$KCHOST/realms/$REALM/protocol/openid-connect/token"  | jq -r '.access_token'`
+echo "$ACCESS_TOKEN"

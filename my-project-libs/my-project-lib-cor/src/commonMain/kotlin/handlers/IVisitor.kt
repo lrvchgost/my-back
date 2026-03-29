18b0ecaa6597a688905b04c133beeb4842bde6ed
Roman Lonskii <lrvchgost@gmail.com>
2026-03-29 17:04:11 +0300

    fix mr

--- my-project-libs/my-project-lib-cor/src/commonMain/kotlin/handlers/IVisitor.kt
+++
@@ -1,6 +0,0 @@
-package ru.otus.otuskotlin.lrvch.libs.cor.handlers
-
-interface IVisitor<T> {
-    suspend fun visitForWorker(node: CorWorker<T>, context: T)
-    suspend fun visitForChain(node: CorChain<T>, context: T)
-}
